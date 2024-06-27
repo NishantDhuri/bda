518272ee6f57b9bd675ec31e53c17715d25322c3
NishantDhuri <107022906+NishantDhuri@users.noreply.github.com>
2024-06-27 23:50:33 +0530

    Add files via upload

---
+++ prac_2_mongodb.py
@@ -0,0 +1,8 @@
+import pymongo
+mongo_uri = "mongodb://localhost:27017/"
+client = pymongo.MongoClient(mongo_uri)
+print(client.list_database_names())
+db = client.my_database
+print(db.list_collection_names())
+table=db.books
+print(table.count_documents({}))
